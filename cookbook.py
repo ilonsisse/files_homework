dc70e4ab312ac81c8e53bc3205193bfe78fe2583
Evgenii Istomin <ilonsisse@yandex.ru>
2024-03-29 22:34:49 +0300

    func read_cookbook works

--- cookbook.py
+++ cookbook.py
@@ -1,24 +1,33 @@
 class Cookbook:
 
     def read_cookbook(self):
+        """ This function reads file line by line and returns file data as cook_book dictionary"""
         with open('recipes.txt', 'r') as recipes:
             cook_book = {}
-            ingredients = []
 
-            for line in recipes:
-                dish_name = line.strip()
+            while True:  # Обработка файла с неизвестным количество строк
+                dish_name = recipes.readline().strip()  # strip() удаляет лишние пробелы
+                if not dish_name:  # Выходим из цикла, если дальше пустая строка
+                    break
+
                 ingredients_number = int(recipes.readline().strip())
+                ingredients = []
+
                 for ingredient in range(ingredients_number):
-                    ingredient_summary = recipes.readline().strip().split(sep='|')
+                    ingredient_summary = recipes.readline().strip().split(sep='|')  # split(sep='symbol') разделяет
+                    # строку по символу и возвращает список
                     ingredients.append({
-                        'ingredient_name': ingredient_summary[0],
-                        'quantity': ingredient_summary[1],
-                        'measure': ingredient_summary[2]
+                        'ingredient_name': ingredient_summary[0].strip(),
+                        'quantity': int(ingredient_summary[1].strip()),
+                        'measure': ingredient_summary[2].strip()
                     })
 
-            recipes.readline()
-            cook_book[dish_name] = ingredients
-            return cook_book
+                cook_book[dish_name] = ingredients
+
+                # Пропускаем пустую строку между рецептами
+                recipes.readline()
+
+        return cook_book
 
 
 my_cookbook = Cookbook()
