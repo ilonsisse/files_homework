18980d655747b8bd93cf7e4fee24be2c26c865b7
Evgenii Istomin <ilonsisse@yandex.ru>
2024-03-29 21:36:14 +0300

    first try of read_cookbook func

--- cookbook.py
+++ cookbook.py
@@ -1,4 +1,23 @@
-with open('recipes.txt', 'r') as recipes:
-    cook_book = {}
-    for line in recipes:
-        dish_name = None
+class Cookbook:
+
+    def read_cookbook(self):
+        with open('recipes.txt', 'r') as recipes:
+            cook_book = {}
+            ingredients = []
+
+            for line in recipes:
+                dish_name = line.strip()
+                ingredients_number = int(recipes.readline().strip())
+                for ingredient in range(ingredients_number):
+                    ingredient_summary = recipes.readline().strip().split(sep='|')
+                    ingredients.append({
+                        'ingredient_name': ingredient_summary[0],
+                        'quantity': ingredient_summary[1],
+                        'measure': ingredient_summary[2]
+                    })
+
+            recipes.readline()
+            cook_book[dish_name] = ingredients
+            return cook_book
+
+
