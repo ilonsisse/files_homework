9cd91133d06ad7fe2dd133134e68bd1b89030eb5
Evgenii Istomin <ilonsisse@yandex.ru>
2024-03-30 19:36:45 +0300

    docstrings added

--- 3rd_task.py
+++ 3rd_task.py
@@ -2,22 +2,36 @@ import os
 
 
 class FileManager:
+    """ Manages files for its processing and writing result.
+     Methods: process_files, write_to_result_file.
+
+     """
 
     def __init__(self, directory, files):
+        """Creates FileManager instance.
+         directory: gets files directory
+         files: gets list of file names.
+
+         """
         self.directory = directory
         self.files = files
 
-    def read_file(self, file_path):
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-            return os.path.basename(file_path), len(lines), lines
-
     def process_files(self):
-        file_contents = [self.read_file(filename) for filename in self.files]
+        """Returns list of tuples with each file information."""
+        file_contents = []
+        for filename in self.files:
+            file_path = os.path.join(self.directory, filename)
+            with open(file_path, 'r') as file:
+                lines = file.readlines()
+                file_contents.append((filename, len(lines), lines))
         file_contents.sort(key=lambda x: x[1])
         return file_contents
 
     def write_to_result_file(self, file_contents):
+        """Gets list of tuples with each file information and writes down its information to result.txt in current
+        directory.
+
+        """
         with open('result.txt', 'w') as result_file:
             for filename, num_lines, lines in file_contents:
                 result_file.write(f'{filename}\n{num_lines}\n')
@@ -25,9 +39,9 @@ class FileManager:
                 result_file.write('\n')
 
 
-curr_dir = os.getcwd()
+files_dir = os.path.join(os.getcwd(),'files')
 files = ['1.txt', '2.txt', '3.txt']
 
-file_manager = FileManager(curr_dir, files)
+file_manager = FileManager(files_dir, files)
 file_contents = file_manager.process_files()
 file_manager.write_to_result_file(file_contents)
