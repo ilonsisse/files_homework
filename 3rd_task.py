5e9c899bb3e8d5a28dba6218943df7ef1c1d944c
Evgenii Istomin <ilonsisse@yandex.ru>
2024-03-30 19:06:25 +0300

    code doesn't work

--- 3rd_task.py
+++ 3rd_task.py
@@ -8,10 +8,26 @@ class FileManager:
         self.files = files
 
     def read_file(self, file_path):
-        with open('file_path', 'r') as file:
+        with open(file_path, 'r') as file:
             lines = file.readlines()
             return os.path.basename(file_path), len(lines), lines
 
+    def process_files(self):
+        file_contents = [self.read_file(filename) for filename in self.files]
+        file_contents.sort(key=lambda x: x[1])
+        return file_contents
+
+    def write_to_result_file(self, file_contents):
+        with open('result.txt', 'w') as result_file:
+            for filename, num_lines, lines in file_contents:
+                result_file.write(f'{filename}\n{num_lines}\n')
+                result_file.writelines(lines)
+                result_file.write('\n')
 
 
+curr_dir = os.getcwd()
+files = ['1.txt', '2.txt', '3.txt']
 
+file_manager = FileManager(curr_dir, files)
+file_contents = file_manager.process_files()
+file_manager.write_to_result_file(file_contents)
